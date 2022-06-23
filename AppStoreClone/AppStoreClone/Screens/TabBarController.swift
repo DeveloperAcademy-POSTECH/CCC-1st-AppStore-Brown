84c0c60154a73826632dec73a52421b8c3c018c2
JI SOO <dlwltn815@gmail.com>
2022-06-23 17:31:40 +0900

    Feat: 탭바 아이콘 및 제목 추가

--- AppStoreClone/AppStoreClone/Screens/TabBarController.swift
+++ AppStoreClone/AppStoreClone/Screens/TabBarController.swift
@@ -11,30 +11,40 @@ class TabBarController: UITabBarController {
     
     private lazy var todayViewController: UIViewController = {
         let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "Today", image: UIImage(systemName: "doc.text.image"), tag: 0)
+        viewController.tabBarItem = tabBarItem
         
         return viewController
     }()
     
     private lazy var gamesViewController: UIViewController = {
         let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "Games", image: UIImage(systemName: "gamecontroller.fill"), tag: 0)
+        viewController.tabBarItem = tabBarItem
         
         return viewController
     }()
     
     private lazy var appsViewController: UIViewController = {
         let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "Apps", image: UIImage(systemName: "square.stack.3d.up"), tag: 0)
+        viewController.tabBarItem = tabBarItem
         
         return viewController
     }()
     
     private lazy var arcadeViewController: UIViewController = {
         let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "Arcade", image: UIImage(systemName: "tortoise.fill"), tag: 0)
+        viewController.tabBarItem = tabBarItem
         
         return viewController
     }()
     
     private lazy var searchViewController: UIViewController = {
         let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 0)
+        viewController.tabBarItem = tabBarItem
         
         return viewController
     }()
