04ec6858b08fac8572ef34399075185dae3b45b9
JI SOO <dlwltn815@gmail.com>
2022-06-23 13:45:10 +0900

    Feat: TabBar 생성 (#3)

--- AppStoreClone/AppStoreClone/Screens/TabBarController.swift
+++ AppStoreClone/AppStoreClone/Screens/TabBarController.swift
@@ -8,10 +8,41 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+    
+    private lazy var todayViewController: UIViewController = {
+        let viewController = UIViewController()
+        
+        return viewController
+    }()
+    
+    private lazy var gamesViewController: UIViewController = {
+        let viewController = UIViewController()
+        
+        return viewController
+    }()
+    
+    private lazy var appsViewController: UIViewController = {
+        let viewController = UIViewController()
+        
+        return viewController
+    }()
+    
+    private lazy var arcadeViewController: UIViewController = {
+        let viewController = UIViewController()
+        
+        return viewController
+    }()
+    
+    private lazy var searchViewController: UIViewController = {
+        let viewController = UIViewController()
+        
+        return viewController
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewControllers = [todayViewController, gamesViewController, appsViewController, arcadeViewController, searchViewController]
     }
 
 
