fa4b56b5e2e2f99b3c803c7f37fb3ef8ba31cea7
JI SOO <dlwltn815@gmail.com>
2022-06-26 08:22:13 +0900

    Feat: cell의 label과 imageView 생성 (#5)

--- AppStoreClone/AppStoreClone/Screens/Today/TodayCollectionViewCell.swift
+++ AppStoreClone/AppStoreClone/Screens/Today/TodayCollectionViewCell.swift
@@ -10,6 +10,41 @@ import UIKit
 import SnapKit
 
 final class TodayCollectionViewCell: UICollectionViewCell {
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 12.0
+        imageView.backgroundColor = .gray
+        
+        return imageView
+    }()
+    
     func temp() {
         backgroundColor = .black
     }
