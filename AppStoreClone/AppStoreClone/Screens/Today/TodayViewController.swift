6fa8ea73ea97a80a060565865f0681d59d49509e
JI SOO <dlwltn815@gmail.com>
2022-06-27 15:32:26 +0900

    Feat: 투데이 뷰 제목 생성 (#5)

--- AppStoreClone/AppStoreClone/Screens/Today/TodayViewController.swift
+++ AppStoreClone/AppStoreClone/Screens/Today/TodayViewController.swift
@@ -11,6 +11,37 @@ import SnapKit
 
 final class TodayViewController: UIViewController {
     
+    private lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label.text = "MONDAY 27 JUNE"
+        label.textColor = .lightGray
+        
+        return label
+    }()
+    
+    private lazy var todayLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 36, weight: .bold)
+        label.text = "Today"
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    private lazy var userImageView: UIImageView = {
+        let image = UIImageView()
+        image.backgroundColor = .clear
+        image.contentMode = .scaleAspectFill
+        image.layer.cornerRadius = 17.5
+        image.layer.borderWidth = 0.25
+        image.layer.borderColor = UIColor.lightGray.cgColor
+        image.image = UIImage(named: "profile")
+        image.clipsToBounds = true
+        
+        return image
+    }()
+    
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
